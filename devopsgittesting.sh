aa6e8d3689216cac8ee29c697effff9074068d17
root <root@DESKTOP-8BCUH8T.localdomain>
2024-07-26 00:48:33 +0530

    firsttestingdevops

---
+++ devopsgittesting.sh
@@ -0,0 +1,17 @@
+commands:
+
+git add, git commit , git push , git pull 
+
+.git --- HEAD , config , hooks ,Obejects ,refs 
+
+git add calculator.sh 
+
+git checkout cal
+
+git log 
+
+git reset --hard commit ID
+
+git reset --hard 6c309e2cade389e7a0a4e315acac3bdab1f654ce
+
+
